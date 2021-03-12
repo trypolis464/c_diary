e6ec5f3cb980ec77b6f7a46a2e66c047de634b9d
trypolis <60036684+TyGillespie@users.noreply.github.com>
2021-03-12 15:48:22 -0700

    .txt is now added to the end of the filename automatically.

--- c_diary.c
+++ c_diary.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	char name[200];
 	printf("Enter the name of this entry.\n");
 	scanf("%s", &name);
+	strcat(name, ".txt");
 	FILE *file = fopen(name, "w");
 	char text[1000];
 	printf("Enter text.");
